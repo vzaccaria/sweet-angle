d0bcee3dab2558b480c340740864d54b8052a677
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2014-04-02 16:22:37 +0200

    working index.html

---
+++ example.js
@@ -0,0 +1,38 @@
+angular.module('application').directive('progressBar', function () {
+    console.log(arguments);
+    var ret = {};
+    ret.restrict = 'A';
+    ret.template = '<div><link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/0.13.0/css/semantic.css"/><div ng-class="[visibility, color]" class="ui progress transition active"><div style="{{bsize}}" class="bar"></div></div></div>';
+    ret.replace = true;
+    ret.scope = {};
+    ret.scope.value = '=';
+    ret.scope.color = '@';
+    ret.scope.max = '@';
+    ret.link = function (s) {
+        (function (elem, attr) {
+            this.visibility = 'hidden';
+            this.elem = elem;
+            this.toggle = function () {
+                if (this.visibility === 'hidden') {
+                    this.visibility = '';
+                } else {
+                    this.visibility = 'hidden';
+                }
+                this.onValueChange();
+            }.bind(this);
+            this.onValueChange = function () {
+                var perc;
+                perc = this.value / parseFloat(this.max) * window.jQuery(this.elem).width();
+                this.bsize = 'width: ' + perc + 'px;';
+            }.bind(this);
+            this.$watch('value', this.onValueChange);
+        }.apply(s, Array.prototype.slice.call(arguments, 1)));
+    };
+    return ret;
+});
+window.$$$ = function (el) {
+    return angular.element(document.querySelector(el)).isolateScope();
+};
+window.$$ = function (sel) {
+    return angular.element(sel).scope();
+};
