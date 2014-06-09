4cc351ba0891e45fad7b7b472be8b3c0cbf0aafc
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2014-06-09 16:53:27 +0200

    added example files

---
+++ test/maketest.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+
+
+# Get basename:
+# use bn = `get_base_name xyx..`
+
+function get_base_name {
+	n=$1
+	n=$(basename "$n")
+	echo "${n%.*}"
+}
+
+function check_if_failed {
+	if [[ $1 -eq 0 ]]
+	then
+	   echo "OK"
+	else
+	   echo "KO"
+	fi
+}
+
+#
+# Current directory
+#
+srcdir=`dirname $0`
+srcdir=`cd $srcdir; pwd`
+
+
+dstdir=`pwd`
+
+
+for i in $srcdir/*.sa; do
+	f=`get_base_name $i`
+	echo ""
+	echo ""
+	echo "--> $i"
+	$srcdir/../compile.js $srcdir/$f.sa > $srcdir/$f.sar
+done
